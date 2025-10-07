23dcfcec55d328b195fa44a14f75d78c95a267b7
marulebed <maru.lebed@yandex.kz>
2025-10-07 20:51:25 +0400

    add create file

--- lab1.py
+++ lab1.py
@@ -9,21 +9,29 @@ birthday = ''
 phone = ''
 city = ''
 k = 0
-for i in text:
-	if re.search('Фамилия: ', i) != None:
-		lastname = re.sub('\n','',re.sub('Фамилия: ','',i))
-	elif re.search('Имя: ', i) != None:
-		firstname = re.sub('\n','',re.sub('Имя: ','',i))
-	elif re.search('Пол: ', i) != None:
-		sex = re.sub('\n','',re.sub('Пол: ','',i))
-	elif re.search('Дата рождения: ', i) != None:
-		birthday = re.sub('\n','',re.sub('Дата рождения: ','',i))
-	elif re.search('Номер телефона или email: ', i) != None:
-		phone = re.sub('\n','',re.sub('Номер телефона или email: ','',i))
-	elif re.search('Город: ', i) != None:
-		city = re.sub('\n','',re.sub('Город: ','',i))
-	elif re.search(r'\d\)', i) == None:
-		if sex == 'Ж' or sex == 'ж' or sex == 'Женский' or sex == 'женский':
-			if firstname[0] == 'А' or firstname[0] == 'a':
-				k = k+1
+with open("newdata.txt", "w") as file:
+	for i in text:
+		if re.search('Фамилия: ', i) != None:
+			lastname = re.sub('\n','',re.sub('Фамилия: ','',i))
+		elif re.search('Имя: ', i) != None:
+			firstname = re.sub('\n','',re.sub('Имя: ','',i))
+		elif re.search('Пол: ', i) != None:	
+			sex = re.sub('\n','',re.sub('Пол: ','',i))
+		elif re.search('Дата рождения: ', i) != None:
+			birthday = re.sub('\n','',re.sub('Дата рождения: ','',i))
+		elif re.search('Номер телефона или email: ', i) != None:
+			phone = re.sub('\n','',re.sub('Номер телефона или email: ','',i))
+		elif re.search('Город: ', i) != None:
+			city = re.sub('\n','',re.sub('Город: ','',i))
+		elif re.search(r'\d\)', i) == None:
+			if sex == 'Ж' or sex == 'ж' or sex == 'Женский' or sex == 'женский':
+				if firstname[0] == 'А' or firstname[0] == 'a':
+					k = k+1
+					file.write(str(k)+')\n')
+					file.write('Фамилия: '+lastname+'\n')
+					file.write('Имя: '+firstname+'\n')
+					file.write('Пол: '+sex+'\n')
+					file.write('Дата рождения: '+birthday+'\n')
+					file.write('Номер телефона или email: '+phone+'\n')
+					file.write('Город: '+city+'\n\n')
 print(k)
